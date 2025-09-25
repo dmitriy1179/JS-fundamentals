0926da8153f581527ee7ac88ab4d93035570bf4a
dmitriy1179 <chda7911@gmail.com>
2025-09-25 18:36:34 +0300

    add  cycle in 1.2

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ let name = prompt("What your name?");
 alert(`Hello, ${name}`);
 
 //1.2
-let yearOfBirth = prompt("What year were you born?");
+let yearOfBirth;
 const currentYear = Number(2025);
-Number.isNaN(+yearOfBirth) || Number.isNaN(parseInt(yearOfBirth)) || (yearOfBirth > 2025) ? alert("You entered an incorrect year of birth") : alert(`You are ${currentYear - yearOfBirth} years old`);
+while(Number.isNaN(+yearOfBirth) || Number.isNaN(parseInt(yearOfBirth)) || (yearOfBirth > 2025)) {
+yearOfBirth = prompt("What year were you born?");
+}
+alert(`You are ${currentYear - yearOfBirth} years old`);
 
 //1.3 
 let widthSquare = prompt("What is width of square?");
