5c9f6fb8078ae00511e522f4e12bfe727aee6be7
dmitriy1179 <chda7911@gmail.com>
2025-09-25 18:22:36 +0300

    add second  hw

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let name = prompt("What your name?");
 alert(`Hello, ${name}`);
 
 //1.2
-let yearOfBirth = prompt("What /*  */year were you born?");
+let yearOfBirth = prompt("What year were you born?");
 const currentYear = Number(2025);
 Number.isNaN(+yearOfBirth) || Number.isNaN(parseInt(yearOfBirth)) || (yearOfBirth > 2025) ? alert("You entered an incorrect year of birth") : alert(`You are ${currentYear - yearOfBirth} years old`);
 
@@ -11,3 +11,34 @@ Number.isNaN(+yearOfBirth) || Number.isNaN(parseInt(yearOfBirth)) || (yearOfBirt
 let widthSquare = prompt("What is width of square?");
 Number.isNaN(+widthSquare) || Number.isNaN(parseInt(widthSquare)) ? alert("You entered an incorrect data") :
 alert(`Perimeter is ${widthSquare *= 4}`);
+
+//2.1
+let a = (0.1 + 0.2).toFixed(1);
+console.log("a", a);
+
+//2.2
+let str = "1";
+let nbr = 2;
+console.log("result:", +str + nbr);
+
+//2.3
+let memory;
+do {
+  const response = prompt("Specify the memory capacity in GB");
+  memory = +response && parseInt(response);
+} while(!memory);
+let files = Math.floor(memory * 1024 / 820);
+alert(`The flash drive can hold ${files} files`)
+
+
+//2.4
+let amount;
+do {
+  const response = prompt("enter the deposit amount");
+  amount = +response && parseInt(response);
+} while(!amount);
+const percent = 5;
+const month = 2;
+const result = (amount * (percent / 12 * month / 100)).toFixed(2);
+const [grn, kop] = result.split(".");
+alert(`${grn} grn ${kop} kop`);
